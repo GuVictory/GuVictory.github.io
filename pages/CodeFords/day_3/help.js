3ccf426e65e49ab09c16170b78d029f084cd4818
GuVictory <gubanova.vi10@gmail.com>
2020-10-09 02:18:55 +0500

    day_3 task added

---
+++ pages/CodeFords/day_3/help.js
@@ -0,0 +1,17 @@
+const helpTabChange = (event, tabName) => {
+
+    let i, tabcontent, tablinks;
+    
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks-js");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "flex";
+    event.currentTarget.className += " active";
+};
